297638bda0651291ad10bef6734b95abb9e61400
florence <loracodes3@gmail.com>
2024-09-27 13:22:49 +0300

    string concetenation and interpolation

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ console.log(`${str} ${str.length}`);
 //The ${str} inserts the value of str, and ${str.length} inserts the length of the string.
 //The string str is: 'Life, the universe and everything. Answer:'
 //The length of this string is 42, because there are 42 characters (including spaces and punctuation).
+
+//you can also use console.log(str + str.length); to output the same answer
