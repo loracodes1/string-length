ed5126e202b52fd0496044654faf107e1b0004e2
florence <loracodes3@gmail.com>
2024-09-27 13:27:29 +0300

    commit

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ console.log(`${str} ${str.length}`);
 //The string str is: 'Life, the universe and everything. Answer:'
 //The length of this string is 42, because there are 42 characters (including spaces and punctuation).
 
-//you can also use console.log(str + str.length); to output the same answer
+//you can also use console.log(str + str.length); to output the same answer (string concatenation)
+//or you can also use cocole.log(str, str.length) to output the same value (multiple arguments in console log)
