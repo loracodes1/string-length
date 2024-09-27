1601b626f48aaf74a7a3975cdcb57f0362a197df
florence <loracodes3@gmail.com>
2024-09-27 13:14:09 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const str='Life, the universe and everything. Answer:';
+console.log(`${str} ${str.length}`);
+
+//The ${str} inserts the value of str, and ${str.length} inserts the length of the string.
